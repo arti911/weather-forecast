6de58e14094ba921c1aba8fffc840185c8c7c46e
Artur <artur.jog@yandex.rum>
2020-10-01 20:39:21 +0400

    Updates

---
+++ precache-manifest.2e0d733253eec1e81da7d99d37434edc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fda75fed260d41c51aaa3b1965b2c4",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "902cf3bfc7fbe013260c",
+    "url": "/weather-forecast/static/css/2.c3565db1.chunk.css"
+  },
+  {
+    "revision": "3be0e03ade0ec24d4200",
+    "url": "/weather-forecast/static/css/main.fb3fdd82.chunk.css"
+  },
+  {
+    "revision": "902cf3bfc7fbe013260c",
+    "url": "/weather-forecast/static/js/2.76489c93.chunk.js"
+  },
+  {
+    "revision": "516b28df362d3571e58b9d26278408b5",
+    "url": "/weather-forecast/static/js/2.76489c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be0e03ade0ec24d4200",
+    "url": "/weather-forecast/static/js/main.d187eae8.chunk.js"
+  },
+  {
+    "revision": "675f4ddeb73fadf0473e",
+    "url": "/weather-forecast/static/js/runtime-main.f2cddf36.js"
+  },
+  {
+    "revision": "9f6dd7fcea2542fdcbb7207fdf0affc4",
+    "url": "/weather-forecast/static/media/calendar.9f6dd7fc.png"
+  }
+]);
