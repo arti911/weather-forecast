3e50ddad90a40585158ea4bd7fd983be15a999f2
Artur Papikian <34072449+arti911@users.noreply.github.com>
2022-01-21 17:58:36 +0300

    added constants api key & url endpoint

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const FREE_CURRENCY_URL = "https://freecurrencyapi.net/api/v2/";
+export const FREE_CURRENCY_API_KEY = "8491c930-79ff-11ec-8840-b9c5716db3d3";
+
+export const RATES_URL = "//api.exchangeratesapi.io/v1/";
+export const RATES_API_KEY = "b62f598f6af35d8db74f3e6c25a92139";
+
+export const WEATHER_URL = "//api.openweathermap.org/data/2.5/forecast";
+export const WEATHER_API_KEY = "c4421a76df1ebaf51fc8f1042a0fd8de";
